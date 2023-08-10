bc492d21b83a4191d18752dbcafd6f77385d1d15
kyunghee kwak <kwakkyunghee325@gmail.com>
2023-08-10 09:14:31 +0900

    Product클래스 추가

---
+++ Steam2/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam2
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public int Buy { get; set; }
+    }
+}
